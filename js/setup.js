c879c6b454c6f5e50c6bf7b3c4df35080d922da7
o-strekalova <strekalova.olg@yandex.ru>
2020-02-02 01:20:21 +0300

    Отрисовывает похожих волшебников

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,8 @@
 
 var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
 
 var userDialog = document.querySelector('.setup');
 userDialog.classList.remove('hidden');
@@ -12,36 +14,36 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
+var getRandomItem = function (array) {
+  var x = Math.floor(Math.random() * Math.floor(array.length));
+  return array[x];
+};
 
-/* цикл makeWizard var wizards = [
-  {
-    name: WIZARD_NAMES[0],
-    coatColor: 'rgb(241, 43, 107)',
-    eyeColor:
-  },
-  {
-    name: WIZARD_NAMES[1],
-    coatColor: 'rgb(215, 210, 55)'
-  },
-  {
-    name: WIZARD_NAMES[2],
-    coatColor: 'rgb(101, 137, 164)'
-  },
-  {
-    name: WIZARD_NAMES[3],
-    coatColor: 'rgb(127, 127, 127)'
+var makeWizardArray = function (number, firstNames, lastNames, coatColors, eyesColors) {
+  var wizardArray = [];
+
+  for (var i = 0; i < number; i++) {
+    var wizard = {};
+    wizard.name = getRandomItem(firstNames) + ' ' + getRandomItem(lastNames);
+    wizard.coatColor = getRandomItem(coatColors);
+    wizard.eyesColor = getRandomItem(eyesColors);
+    wizardArray[i] = wizard;
   }
-];*/
 
+  return wizardArray;
+};
+
+var wizards = makeWizardArray(4, WIZARD_FIRST_NAMES, WIZARD_LAST_NAMES, WIZARD_COAT_COLOR, WIZARD_EYES_COLOR);
 
 var renderWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < wizards.length; i++) {
